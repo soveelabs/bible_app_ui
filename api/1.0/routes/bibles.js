5ba1d5e96501f65a1d4910342990cda0001b6b0f
rahul samaddar <rahul.samaddar@jktech.com>
2015-12-23 16:51:12 +0530

    implemented angular material for bible listing api

--- api/1.0/routes/bibles.js
+++ api/1.0/routes/bibles.js
@@ -13,21 +13,33 @@ var routes = {
       if (err) { return res.status(500).json({error: err}); }
 
       if (bible_res.statusCode == 200) {
-        return res.json(bible_res);
+        var processedRes = JSON.parse(bible_res.body);
+        return res.json(processedRes);
       } else {
         return res.status(bible_res.statusCode).json(bible_res.body);
       }
     });
-  }//,
-//  books: function (req, res, next) {
-//    var q = req.query;
-//  }
+  },
+  books: function (req, res, next) {
+    var q = req.query;
+    console.log(q.bible_id + "i am here ");
+    BibleAPI.getBooks(config.auth_token, q.bible_id, function(err, books_res) {
+      if (err) { return res.status(500).json({error: err}); }
+
+      if (books_res.statusCode == 200) {
+        var processedRes = JSON.parse(books_res.body);
+        return res.json(processedRes);
+      } else {
+        return res.status(books_res.statusCode).json(books_res.body);
+      }
+    });
+  }
 };
 
 
 router.get('/', routes.index);
 //router.get('/:bible_id', routes.index);
-//router.get('/books', routes.books);
+router.get('/books', routes.books);
 //router.get('/chapters', routes.chapters);
 //router.get('/detect', routes.detect);
 
