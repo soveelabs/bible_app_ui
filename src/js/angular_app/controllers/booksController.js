2ca03022ad0d65891a1dc0ce755501045be316c2
rahul samaddar <rahul.samaddar@jktech.com>
2015-12-29 15:45:25 +0530

    integrated books listing api

---
+++ src/js/angular_app/controllers/booksController.js
@@ -0,0 +1,31 @@
+(function() {
+
+  angular.module('obaui')
+    .controller('BooksController', function($scope, $http, $routeParams) {
+
+      window.test26 = $scope;
+      $scope.bible_id = $routeParams.bible_id || '';
+      
+
+      // API path
+      var apiPath = '/api/1.0';
+      var apiUrl = apiPath + '/bibles/books';
+      //apiUrl += $scope.bible_id ? '/' + $scope.bible_id : '';
+      apiUrl += '?token=25142499049aa20261871b2db25eea92a5fe4e72&bible_id=' + $scope.bible_id;
+      
+
+      // ======== HTTP requests
+      $http.get(apiUrl)
+        .success(function(data, status, headers, config) {
+          $scope.books_data = data;
+        })
+        .error(function(data, status, headers, config) {
+          // handle errors
+          console.log('ERROR:: Failed to get /bibles');
+          console.log('ERROR:: Error code: ' + status);
+        });
+
+
+    }); /* END BiblesController */
+
+})();
